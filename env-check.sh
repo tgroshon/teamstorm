74367a19fc527dcde971fcd3b8f8785bff2e6b9f
Tommy Groshong <tgroshong@instructure.com>
2015-03-01 15:21:33 -0700

    Fix circular postinstall bug

--- env-check.sh
+++ env-check.sh
@@ -1,5 +1,11 @@
 if [ "$NODE_ENV" = "production" ]
 then
+  if [ -d "node_modules/gulp" ]; then
+    echo "Build Deps present"
+    exit 0
+  fi
   npm run autobuild
 fi
+
+echo "Skipping Build Deps on Non-prod environment"
 exit 0
