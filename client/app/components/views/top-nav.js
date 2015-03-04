bb8b44f8d7e744a0aa3a4061273e8c24bf56904f
Tommy Groshong <tgroshong@instructure.com>
2015-03-03 22:19:40 -0700

    Client: Updateable Navigation Items

--- client/app/components/views/top-nav.js
+++ client/app/components/views/top-nav.js
@@ -17,6 +17,7 @@ var CustomItem = React.createClass({
 })
 
 export default React.createClass({
+  displayName: 'TopNavigation',
   mixins: [Navigation],
 
   getInitialState() {
@@ -43,16 +44,8 @@ export default React.createClass({
     this.transitionTo('login')
   },
 
-  handleLogin(evt) {
-    evt.preventDefault()
-    this.transitionTo('login')
-  },
-
   getUserAuthLink() {
     if (this.state.user) {
-      return (
-        <MenuItem onClick={this.handleLogout}>Logout</MenuItem>
-      )
     } else {
       return (
         <MenuItem onClick={this.handleLogin}>Login</MenuItem>
@@ -60,16 +53,18 @@ export default React.createClass({
     }
   },
 
-  getDropdown() {
-    return (
-      <DropdownButton key={5} title="Dropdown">
-        <MenuItem eventKey="1">Action</MenuItem>
-        <MenuItem eventKey="2">Another action</MenuItem>
-        <MenuItem eventKey="3">Something else here</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey="4">Separated link</MenuItem>
-      </DropdownButton>
-    )
+  getNavItems() {
+    if (this.state.authenticated) {
+      return [
+        <CustomItem key={2} to="teams">Teams</CustomItem>,
+        <CustomItem key={3} to="activities">Activities</CustomItem>,
+        <MenuItem key={4} onClick={this.handleLogout}>Logout</MenuItem>
+      ]
+    } else {
+      return (
+        <CustomItem key={4} to="login">Login</CustomItem>
+      )
+    }
   },
 
   render() {
@@ -77,10 +72,7 @@ export default React.createClass({
       <Navbar className="navbar-fixed-top">
         <Nav>
           <div key={1} className="navbar-brand">TeamStorm</div>
-          <CustomItem key={2} to="teams">Teams</CustomItem>
-          <CustomItem key={3} to="activities">Activities</CustomItem>
-          {this.getUserAuthLink()}
-          {this.getDropdown()}
+          {this.getNavItems()}
         </Nav>
       </Navbar>
     )
